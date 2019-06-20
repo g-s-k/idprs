7d2adac94d82456ade2ae86833f69d949b955914
George Kaplan <george@georgekaplan.xyz>
2019-06-20 02:06:29 -0400

    streamline flow and add errors

--- src/index.js
+++ src/index.js
@@ -18,18 +18,30 @@ function* getIdents(text) {
       selLen++;
     }
 
+    let sel = text.slice(0, selLen).trimEnd();
+    text = text.slice(selLen + 1).trimStart();
+
+    if (text.length === 0) {
+      throw Error(`Identifier \`${sel}\` did not have angle brackets`);
+    }
+
     let relLen = 0;
-    for (let c of text.slice(selLen + 1)) {
+    for (let c of text) {
       if (c === ">") break;
       relLen++;
     }
 
-    let sel = text.slice(0, selLen).trim(),
-      rel = text.slice(selLen + 1, selLen + 1 + relLen).trim();
+    let rel = text.slice(0, relLen).trimEnd();
+
+    if (relLen === text.length) {
+      throw Error(
+        `Identifier \`${sel}<${rel}\` did not have a closing angle bracket`
+      );
+    }
+
+    text = trimDelim(text.slice(relLen + 1));
 
     yield `${sel}<${rel}>`;
-
-    text = trimDelim(text.slice(selLen + relLen + 2));
   }
 }
 
@@ -39,7 +51,13 @@ if (typeof require !== "undefined" && require.main === module) {
   var fs = require("fs");
   var ids = fs.readFileSync(0, "utf-8");
 
-  for (let token of getIdents(ids)) {
-    console.log(token);
+  try {
+    for (let ident of getIdents(ids)) {
+      console.log(ident);
+    }
+  } catch (e) {
+    console.error("Error encountered while parsing.");
+    console.error(e);
+    process.exit(1);
   }
 }
